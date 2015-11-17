ecd2392d9670f53c7d5d47689ec83531e4ed2939
Peter <Peter@users.noreply.github.com>
2015-11-17 17:20:38 +0800

    --message=add a mod for item in list

---
+++ assets/js/modules/list-item-index.js
@@ -0,0 +1,45 @@
+define(['vue'], function (Vue) {
+  // ------------------------------------
+  // return function (id, data) {
+  //   return new Vue({
+  //     el : id,
+  //     data : data
+  //   });
+  // }
+  // ------------------------------------
+
+  // #demo
+
+  var component = Vue.component('list-item', {
+    template : '#list-item',
+    data : function () {
+      return {
+      }
+    }
+  });
+
+  var posList = new Vue({
+    el: '#my-position-info-list',
+    data: {
+      items : [
+        {
+          desc : { name : "翠花", position: "销售部总监", company: "IBM" },
+          recommending : [
+            { name : "圣诞树", isNew : true, level1 : 1, level2: 4  },
+            { name : "圣诞树", isNew : true, level1 : 1, level2: 4  },
+            { name : "圣诞树", isNew : true, level1 : 1, level2: 4  },
+            { name : "圣诞树", isNew : true, level1 : 1, level2: 4  },
+          ],
+          matched : [
+            { name : "圣诞树2", isNew : true, level1 : 2, level2: 3  },
+            { name : "圣诞树2", isNew : true, level1 : 2, level2: 3  },
+            { name : "圣诞树2", isNew : true, level1 : 2, level2: 3  },
+            { name : "圣诞树2", isNew : true, level1 : 2, level2: 3  }
+          ],
+          update : { ymd : '2015/10/30', hms : '16:00' }
+        },
+      ]
+    }
+  });
+  posList.$append(component);
+})
